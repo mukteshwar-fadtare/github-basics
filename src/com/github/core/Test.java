5efcfc99c8296e8fac472969e92b0db024b57806
Fadtare <mukteshwar.fadtare@atos.net>
2022-03-08 18:47:05 +0530

    msg added

--- src/com/github/core/Test.java
+++ src/com/github/core/Test.java
@@ -5,5 +5,6 @@ public class Test {
     public static void main(String[] args) {
 
         System.out.println("test class");
+        System.out.println("changes merged from main branch...");
     }
 }
